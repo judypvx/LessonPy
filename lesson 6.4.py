e636243486a6b649287c93d6199b36514a1074ca
Oledjy <sources.official@gmail.com>
2024-06-28 13:29:21 +0300

    Lesson 6.4

--- lesson 6.4.py
+++ lesson 6.4.py
@@ -4,3 +4,22 @@
 # считает и выводит на консоль среднее арифметическое всех чисел из отрезка [a; b], кратных числу c.
 # Рекомендации
 # Функция range(start, stop) не включает границу stop, останавливается, не доходя до неё.
+
+
+
+a = int(input("Enter a number A: "))
+b = int(input("Enter a number B: "))
+c = int(input("Enter a number C: "))
+
+count_medium = 0
+count_number = 0
+
+
+for number in range (a,b+1):
+    if number % c == 0:
+        count_number += 1
+        count_medium += number
+
+print(count_medium/count_number)
+
+
