8cd359b8a3b0b34f3752b670f6697911312d1d9f
Oledjy <sources.official@gmail.com>
2024-07-03 14:23:56 +0300

    lesson 8.3

---
+++ lesson 8.3.py
@@ -0,0 +1,11 @@
+# Задача 3. Анализ данных
+# Отдел анализа данных сегодня получил вот такую интересную штуку:
+# https://prnt.sc/dKDS_a08LhGP
+
+
+for row in range(10):
+
+    for col in range(10):
+        print(-col + row, end='\t')
+
+    print()
